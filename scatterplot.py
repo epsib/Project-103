6fb6f4d9d61affddd4cbc00314906dd3f1197965
epsib <70732699+epsib@users.noreply.github.com>
2021-11-26 12:49:43 -0500

    Add files via upload

---
+++ scatterplot.py
@@ -0,0 +1,11 @@
+from re import X
+import pandas as pd 
+
+import plotly.express as px 
+
+df = pd.read_csv("data.csv")
+fig = px.scatter(df,    x="Date", 
+                        y = "Cases", 
+                        color="Country"
+                )
+fig.show()
